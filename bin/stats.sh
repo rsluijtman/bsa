1f1f17e28b6ed7ca37556b926a7ddb7f5ab0390b
rws <rws@sluijtman.space>
2017-06-29 12:22:02 +0200

    compare iptables and marked blocked

--- bin/stats.sh
+++ bin/stats.sh
@@ -9,10 +9,40 @@ path=${path%/*}
 cd $ntdir
 b=$(ls */blocked | while read a ; do echo ${a%/blocked} ; done)
 
-echo $b
+#echo $b
+echo marked blocked
 ls -d $b
 
 i=$($iptables -nL ssh | awk '/DROP/{print $4}')
 
-echo $i
+#echo $i
+echo in iptables
 ls -d $i
+
+
+for a in $b 
+do
+  found=0
+  for j in $i
+  do
+    [ $a = $j ] && { found=1 ; continue ; }
+  done
+  if [ $found = 0 ]
+  then 
+    echo $a not in iptables
+  fi
+done
+
+
+for j in $i
+do
+  found=0
+  for a in $b 
+  do
+    [ $a = $j ] && { found=1 ; continue ; }
+  done
+  if [ $found = 0 ]
+  then 
+    echo $j not marked blocked
+  fi
+done
